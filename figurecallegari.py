d81a796b3a9573dfa89d2a59ecf67823c241eda3
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-12-15 18:58:58 -0300

    Update figurecallegari.py

--- figurecallegari.py
+++ figurecallegari.py
@@ -388,22 +388,23 @@ with tab1:
                     t = t_new - 6.5
             
                     
-                    fig_v, ax_v = plt.subplots(figsize=(10, 6))
-                    ax_v.plot(t, norma_gyro, 'k-', label='Vertical')
-                    ax_v.set_xlabel("Tempo (s)")
-                    ax_v.set_ylabel("Velocidade angular (ML)")
-                    ax_v.legend(loc="lower left")
-                    ax_v.set_xlim([5,20])
+                    fig_g, ax_g = plt.subplots(figsize=(10, 6))
+                    ax_g.plot(t, norma_gyro, 'k-', label='Vertical')
+                    ax_g.set_xlabel("Tempo (s)")
+                    ax_g.set_ylabel("Velocidade angular (ML)")
+                    ax_g.legend(loc="lower left")
+                    
                     for i in range(num_ciclos):
                         on, of = onset_adj[i], offset_adj[i]
-                        ax_v.axvline(on, ls='--', color='orange', label='Início' if i==0 else "")
-                        ax_v.axvline(of, ls='--', color='green',  label='Fim' if i==0 else "")
-                        ax_v.axvspan(on, of, color='gray', alpha=0.3, label='Teste' if i==0 else "")
-                        if i < len(stand_adj): ax_v.axvline(stand_adj[i], ls='--', color='red',   label='Pico em pé' if i==0 else "")
-                        if i < len(sit_adj):   ax_v.axvline(sit_adj[i],   ls='--', color='black', label='Pico para sentar' if i==0 else "")
+                        ax_g.axvline(on, ls='--', color='orange', label='Início' if i==0 else "")
+                        ax_g.axvline(of, ls='--', color='green',  label='Fim' if i==0 else "")
+                        ax_g.axvspan(on, of, color='gray', alpha=0.3, label='Teste' if i==0 else "")
+                        if i < len(stand_adj): ax_g.axvline(stand_adj[i], ls='--', color='red',   label='Pico em pé' if i==0 else "")
+                        if i < len(sit_adj):   ax_g.axvline(sit_adj[i],   ls='--', color='black', label='Pico para sentar' if i==0 else "")
                     for k, tp in enumerate(peak_adj):
-                        ax_v.axvline(tp, ls='--', color='blue', label='Mínimos' if k==0 else "")
-                    st.pyplot(fig_v)
+                        ax_g.axvline(tp, ls='--', color='blue', label='Mínimos' if k==0 else "")
+                    ax_g.set_xlim([5,20])
+                    st.pyplot(fig_g)
 
             
     else:
